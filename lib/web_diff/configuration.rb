3fab82840564ec38bbf7969c46d2fcf4079d9837
Ilya Rubnich <irubnich@gmail.com>
2014-08-20 11:55:02 -0400

    Add hooks for gallery step

--- lib/web_diff/configuration.rb
+++ lib/web_diff/configuration.rb
@@ -9,7 +9,7 @@ module WebDiff
     end
 
     class Configuration
-        attr_accessor :_before_run, :_after_run
+        attr_accessor :_before_run, :_after_run, :_before_gallery, :_after_gallery
         attr_accessor :distortion_metric, :highlight_color, :lowlight_color
         attr_accessor :remote, :remote_path, :aws_key, :aws_secret
 
@@ -29,6 +29,22 @@ module WebDiff
                 @_after_run = block
             else
                 @_after_run.call if @_after_run
+            end
+        end
+
+        def before_gallery(&block)
+            if block_given?
+                @_before_gallery = block
+            else
+                @_before_gallery.call if @_before_gallery
+            end
+        end
+
+        def after_gallery(&block)
+            if block_given?
+                @_after_gallery = block
+            else
+                @_after_gallery.call if @_after_gallery
             end
         end
     end
