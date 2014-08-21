8f2f113eaac1554eaec9f8e2b7ed41133bbe325b
Ilya Rubnich <irubnich@gmail.com>
2014-08-21 12:24:29 -0400

    Fix another path name

--- lib/web_diff/image_handler.rb
+++ lib/web_diff/image_handler.rb
@@ -64,7 +64,7 @@ module WebDiff
                 next if ['.', '..'].include?(file)
                 WebDiff.fog.directories.get("circle-artifacts").files.create(
                     key: "#{WebDiff.configuration.remote_path}/#{width}_#{name}/#{file}",
-                    body: File.open("#{@path}/#{subdir}/#{img}"),
+                    body: File.open("#{@path}/#{width}_#{name}/#{img}"),
                     public: true
                 )
             end
