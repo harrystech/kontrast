ec0a09283ea2b4d8a85e92de7142774f991aabf3
Ilya Rubnich <irubnich@gmail.com>
2014-09-09 20:22:32 -0400

    Implement browser resize with workers Reorganize selenium handler to make it a bit more thread-friendly for the future

--- lib/kontrast/selenium_handler.rb
+++ lib/kontrast/selenium_handler.rb
@@ -1,4 +1,5 @@
 require "selenium-webdriver"
+require "workers"
 
 module Kontrast
     class SeleniumHandler
@@ -24,25 +25,39 @@ module Kontrast
         end
 
         def run_comparison(width, path, name)
-            # Get domains
-            test_host = Kontrast.configuration.test_domain
-            production_host = Kontrast.configuration.production_domain
-
-            # Open test host tab
-            @driver.navigate.to("#{test_host}#{path}")
-            @driver2.navigate.to("#{production_host}#{path}")
-
-            # Resize to given width and total height
-            @driver.manage.window.resize_to(width, @driver.manage.window.size.height)
-            @driver2.manage.window.resize_to(width, @driver2.manage.window.size.height)
-
             # Create folder for this test
             current_output = FileUtils.mkdir("#{@path}/#{width}_#{name}").join('')
 
+            # Open test host tabs
+            navigate(path)
+
+            # Resize to given width and total height
+            resize(width)
+
             # Take screenshot
             Kontrast.configuration.before_screenshot(@driver, @driver2)
-            @driver.save_screenshot("#{current_output}/test.png")
-            @driver2.save_screenshot("#{current_output}/production.png")
+            screenshot(current_output)
         end
+
+        private
+            def navigate(path)
+                # Get domains
+                test_host = Kontrast.configuration.test_domain
+                production_host = Kontrast.configuration.production_domain
+
+                @driver.navigate.to("#{test_host}#{path}")
+                @driver2.navigate.to("#{production_host}#{path}")
+            end
+
+            def resize(width)
+                Workers.map([@driver, @driver2]) do |driver|
+                    driver.manage.window.resize_to(width, driver.manage.window.size.height)
+                end
+            end
+
+            def screenshot(output_path)
+                @driver.save_screenshot("#{output_path}/test.png")
+                @driver2.save_screenshot("#{output_path}/production.png")
+            end
     end
 end
