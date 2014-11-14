efd0b395715b3f96bc54da2eba48474b931fda58
Sunny Ng <sunny@harrys.com>
2014-11-14 11:35:24 -0500

    Prevent from failing builds

--- lib/kontrast/selenium_handler.rb
+++ lib/kontrast/selenium_handler.rb
@@ -35,19 +35,26 @@ module Kontrast
             # Create folder for this test
             current_output = FileUtils.mkdir_p("#{@path}/#{width}_#{name}").join('')
 
-            # Open test host tabs
-            navigate(path)
-
-            # Resize to given width and total height
-            resize(width)
-
-            # Take screenshot
             begin
-                Kontrast.configuration.before_screenshot(@test_driver[:driver], @production_driver[:driver], { width: width, name: name })
-                screenshot(current_output)
-            ensure
-                Kontrast.configuration.after_screenshot(@test_driver[:driver], @production_driver[:driver], { width: width, name: name })
+                # Open test host tabs
+                navigate(path)
+
+                # Resize to given width and total height
+                resize(width)
+
+                # Take screenshot
+                begin
+                    Kontrast.configuration.before_screenshot(@test_driver[:driver], @production_driver[:driver], { width: width, name: name })
+                    screenshot(current_output)
+                ensure
+                    Kontrast.configuration.after_screenshot(@test_driver[:driver], @production_driver[:driver], { width: width, name: name })
+                end
+            rescue Exception => e
+                if Kontrast.configuration.fail_build
+                    raise e
+                end
             end
+
         end
 
         private
