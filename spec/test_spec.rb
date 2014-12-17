7e48952facc8d6b34a8b03e0dede9a08bbb74744
Ilya Rubnich <irubnich@gmail.com>
2014-12-17 11:49:31 -0500

    Add a test spec

---
+++ spec/test_spec.rb
@@ -0,0 +1,20 @@
+describe Kontrast::Test do
+    before :each do
+        Kontrast.describe("test") do |spec|
+            spec.before_screenshot do |test_driver, production_driver, test|
+                "before! #{test_driver} #{production_driver} #{test}"
+            end
+        end
+        Kontrast.configure do |config|
+            config.pages(1280) do |page|
+                page.test "/"
+            end
+        end
+        Kontrast.test_suite.bind_specs
+    end
+
+    it "can run a spec's callback" do
+        test = Kontrast.test_suite.tests.first
+        expect(test.run_callback(:before_screenshot, 1, 2, 3)).to eql("before! 1 2 3")
+    end
+end
